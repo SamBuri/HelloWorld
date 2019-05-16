c32c5e8d82d2587bf7106ca599471585f2c13487
Nomis Wils <nomiswilsk@gmail.com>
2019-05-16 09:51:08 +0300

    Defining people's namees

--- Lab.java
+++ Lab.java
@@ -1,2 +1,13 @@
 Publuic class Lab{
+  private String message
+  public Lab(){
+  }
+ 
+ private String Mukiibi(){
+   return "He is an Idiot"
+}
+
+private String Sam(){
+   return "He is the best guy in the word"
+}
 }
